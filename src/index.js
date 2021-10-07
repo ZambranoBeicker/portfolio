00774907812609903575bd5f71e464732ce9d58f
Beicker Zambrano <beicker998@gmail.com>
2021-10-07 16:33:57 -0500

    Configure webpack for html tailwind and js

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import "tailwindcss/tailwind.css";
+import "./index.css";
+import html from "./index.html";
+
+console.log("IT's working properly");
+
+const funcion = () => console.log("arrow function");
+
+funcion();
---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const HtmlCriticalWebpackPlugin = require("html-critical-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "index.js",
+    path: path.resolve(__dirname, "build"),
+    publicPath: "/",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        loader: "html-loader",
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader"],
+      },
+      { test: /.js$/, use: "babel-loader", exclude: /node_modules/ },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "src/index.html",
+    }),
+    new MiniCssExtractPlugin(),
+    new HtmlCriticalWebpackPlugin({
+      base: path.resolve(__dirname, "build"),
+      src: "index.html",
+      dest: "index.html",
+      inline: true,
+      minify: true,
+      extract: true,
+      width: 1920,
+      height: 1080,
+      penthouse: {
+        blockJSRequests: false,
+      },
+    }),
+  ],
+
+  optimization: {
+    minimizer: [new CssMinimizerPlugin()],
+  },
+
+  devServer: {
+    port: 3000,
+    open: true,
+  },
+};
